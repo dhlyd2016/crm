484a2ea9bbe1160948fab19b114d62506c2cffc0
shuiliunian <1877345136@qq.com>
2020-12-24 12:54:45 +0800

    李彦东是个帅哥

---
+++ src/main/java/com/test/crm/controller/LiyanDong.java
@@ -0,0 +1,14 @@
+package com.test.crm.controller;
+
+/**
+ * @ClassName LiyanDong
+ * @author: Dai
+ * @Description TODO
+ * @date 2020/12/24 12:52
+ * @Version 1.0版本
+ */
+public class LiyanDong {
+    public static void main(String[] args) {
+        System.err.println("李彦东是个大帅哥");
+    }
+}
